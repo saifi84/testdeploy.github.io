7b1442da79f4bd2f21a008223680b5a8bf584f4d
Saif Ul Rehman <63366917+saifi84@users.noreply.github.com>
2022-12-29 14:27:21 +0500

    w
    sw

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:saifi84/testdeploy.github.io.git master:gh-pages
+git push -f git@github.com:saifi84/testdeploy.github.io.git main:gh-pages
 
 cd -
